a5c1e81d5c8d3d73d150b38aa7e9a3a899742577
mayur.kanojiya <kanojiyamayur@gmail.com>
2020-06-24 14:35:20 +0530

    chore: structure for ML

---
+++ shared/DataModels/ImagePrediction.cs
@@ -0,0 +1,14 @@
+using Microsoft.ML.Data;
+using Microsoft.ML.Runtime.Api;
+
+namespace ImageClassification.DataModels
+{
+    public class ImagePrediction
+    {
+        [ColumnName("Score")]
+        public float[] Score;
+
+        [ColumnName("PredictedLabel")]
+        public string PredictedLabel;
+    }
+}
---
+++ train/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using ImageClassification;
+
+namespace train
+{
+    class Program
+    {
+
+        public static string GetAbsolutePath(string relativePath)
+            => FileUtils.GetAbsolutePath(typeof(Program).Assembly, relativePath);
+
+        static void Main(string[] args)
+        {
+            const string assetsRelativePath = @"../../../assets";
+            string assetsPath = GetAbsolutePath(assetsRelativePath);
+
+            Console.WriteLine(assetsPath);
+        }
+    }
+}
